5290351810342f84f823a788645cfc2ba0d06e95
Nik Conwell <nikconwell@gmail.com>
2023-01-02 07:51:40 -0500

    Rust building

---
+++ rust/hello_cargo/target/debug/deps/hello_cargo-153ebdc6969fafdb.d
@@ -0,0 +1,5 @@
+/home/nik/github/try/rust/hello_cargo/target/debug/deps/hello_cargo-153ebdc6969fafdb.rmeta: src/main.rs
+
+/home/nik/github/try/rust/hello_cargo/target/debug/deps/hello_cargo-153ebdc6969fafdb.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello_cargo/target/debug/deps/hello_cargo-f3501fee908f5b04.d
@@ -0,0 +1,5 @@
+/home/nik/github/try/rust/hello_cargo/target/debug/deps/hello_cargo-f3501fee908f5b04.rmeta: src/main.rs
+
+/home/nik/github/try/rust/hello_cargo/target/debug/deps/hello_cargo-f3501fee908f5b04.d: src/main.rs
+
+src/main.rs:
