0503f051a4f176b818815c95dad4d86418ffa69a
Nik Conwell <nikconwell@gmail.com>
2024-01-29 06:25:48 -0500

    Experiment converting addresses to latitude

---
+++ python/latitude/convert.py
@@ -0,0 +1,17 @@
+#!/home/nik/github/try/python/latitude/bin/python
+
+# importing geopy library and Nominatim class
+from geopy.geocoders import Nominatim
+
+# calling the Nominatim tool and create Nominatim class
+loc = Nominatim(user_agent="Geopy Library")
+
+# entering the location name
+getLoc = loc.geocode("172 hartford street natick ma")
+
+# printing address
+print(getLoc.address)
+
+# printing latitude and longitude
+print("Latitude = ", getLoc.latitude, "\n")
+print("Longitude = ", getLoc.longitude)
